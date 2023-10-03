ea114727b606c0abdac2036e9e842495751161b8
Artem Druganov <104306822+Mazilas2@users.noreply.github.com>
2023-10-03 18:26:15 +0000

    Code optimization and refactoring

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 """Приложение Flask"""
 import json
-import requests
 import datetime
+import requests
 from flask import Flask, render_template
 
 app = Flask(__name__)
 ITEMS_PER_PAGE = 20
+now = datetime.datetime.now()
 
 
 def get_data(url):
@@ -14,45 +15,60 @@ def get_data(url):
     return json.loads(response.text)
 
 
-@app.route("/")
+def get_config(path="config.json"):
+    """Получить данные из конфигурационного файла"""
+    try:
+        with open(path, "r", encoding="utf-8") as file:
+            config = json.load(file)
+    except FileNotFoundError:
+        config = {"last_update": "2021-01-01 00:00:00"}
+        with open(path, "w", encoding="utf-8") as file:
+            json.dump(config, file)
+    return config
+
+
+def save_config(config, path="config.json"):
+    """Сохранить данные в конфигурационный файл"""
+    with open(path, "w", encoding="utf-8") as file:
+        json.dump(config, file)
+
+# Create def for checking is update needed
+
+
+def check_update(config):
+    """Проверить, нужно ли обновлять данные"""
+    last_update = config["last_update"]
+    last_update_datetime = datetime.datetime.strptime(
+        last_update, "%Y-%m-%d %H:%M:%S")
+    return (now - last_update_datetime).days > 0
+
+
+def update_data(config, config_path="config.json"):
+    """Обновить данные"""
+    url_count = 'https://pokeapi.co/api/v2/pokemon'
+    count = get_data(url_count)["count"]
+    config["count"] = count
+    config["last_update"] = now.strftime("%Y-%m-%d %H:%M:%S")
+    url = f'https://pokeapi.co/api/v2/pokemon?limit={count}&offset=0'
+    data = get_data(url)["results"]
+    # For each pokemon add them index (number in list) and image url
+    for index, pkmn in enumerate(data):
+        pkmn["index"] = index + 1
+        img_base = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
+        pkmn_id = pkmn['url'].split('/')[-2]
+        pkmn["img_url"] = f"{img_base}{pkmn_id}.png"
+    config["data"] = data
+    # Save data to config file
+    save_config(config, config_path)
+
+
+@app.route("/", defaults={"page": 1})
 @app.route("/<int:page>")
 def main(page=1):
     """Главная страница с пагинацией"""
-    # Check in config file last time of update
-    # If last time of update is more than 24 hours
-    # then update data from API
-    # else use data from config file
-    config_path = "config.json"
-    # If file not exist then create it
-    try:
-        with open(config_path, "r", encoding="utf-8") as file:
-            config = json.load(file)
-    except FileNotFoundError:
-        with open(config_path, "w", encoding="utf-8") as file:
-            config = {"last_update": "2021-01-01 00:00:00"}
-            json.dump(config, file)
-    # Check last update time
-    last_update = config["last_update"]
-    now = datetime.datetime.now()
-    last_update_datetime = datetime.datetime.strptime(last_update, "%Y-%m-%d %H:%M:%S")
-    # If last update was more than 24 hours ago
-    # then update count of pokemons from API
-    # and save it to config file
-    # Also update pokemons data from API
-    if (now - last_update_datetime).days > 0:
-        url_count = 'https://pokeapi.co/api/v2/pokemon'
-        count = get_data(url_count)["count"]
-        config["count"] = count
-        config["last_update"] = now.strftime("%Y-%m-%d %H:%M:%S")
-        url = f'https://pokeapi.co/api/v2/pokemon?limit={count}&offset=0'
-        data = get_data(url)["results"]
-        # For each pokemon add them index (number in list)
-        for index, pkmn in enumerate(data):
-            pkmn["index"] = index + 1
-            pkmn["img_url"] = f"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{pkmn['url'].split('/')[-2]}.png"
-        config["data"] = data
-        with open(config_path, "w", encoding="utf-8") as file:
-            json.dump(config, file)
+    config = get_config()
+    if check_update(config):
+        update_data(config)
     # Get data from config file
     count = config["count"]
     # Get page number from url
@@ -69,5 +85,3 @@ def pokemon(name):
     """Получить данные по имени покемона"""
     pokemon_name = name.capitalize()
     return render_template("pokemon.html", title=pokemon_name)
-
-
